2aaf88ed56c6316ab8b7a6d940b6b78e83d3e75b
Brenton Weaver <brenton.weaver@gmail.com>
2022-03-06 19:34:53 +1100

    tried addeding collums to the the product models and worked on the products routes. wasnt able to test yet

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    }
   },
   {
     sequelize,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,10 +4,26 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+Product.findAll({
+    attributes: ["product_name", "price", "stock", "category_id", "id"],
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      {
+        model: Tag,
+        through: ProductTag,
+        as: "tags",
+      },
+    ],
+  })
+    .then((dbProductData) => res.json(dbProductData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
 });
+
 
 // get one product
 router.get('/:id', (req, res) => {
