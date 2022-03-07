6e93788de051ad8e6c4e6e4da610cf4832b8642a
Brenton Weaver <brenton.weaver@gmail.com>
2022-03-08 00:52:09 +1100

    updated the read me and got rid of unneeded commments

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# eomBackend
+# README: e-commerce backend
+
+## Description:
+This application uses MySql to build a database that can allows CRUD methods to update its tables. It utilizes sequelize.
+
+<br>
+<br>
+
+## Installation:
+Download the repo contents, run the code ***mysql -u root -p*** and follow with your MySql password. 
+Then install the database by running the following ***SOURCE db/schema.sql***.
+
+Then seed the database by exiting your Mysql terminal and running ***node seeds/index.js***.
+
+Install dependencies by running ***NPM i*** and you should be ready to demo the database. Run ***NPM start*** and the server will run on your local host 3001.
+
+<br>
+<br>
+
+|Task| Status |
+|--|--|
+|Repo on GitHub | complete |
+|Use DotEnv NPM | complete |
+|Use MySql and Sequelize | complete |
+|API Routes work | complete |
+|Able to CREATE with post | complete |
+|Able to READ with get method | complete |
+|Able to UPDATE with post method | complete |
+|Able to DELETE with delete method | complete |
+
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,37 +1,31 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
-// get all products
-Product.findAll({
-    attributes: ["product_name", "price", "stock", "category_id", "id"],
-    include: [
-      {
-        model: Category,
-        attributes: ["id", "category_name"],
-      },
-      {
-        model: Tag,
-        through: ProductTag,
-        as: "tags",
-      },
-    ],
-  })
-    .then((dbProductData) => res.json(dbProductData))
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll();
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, {model: Tag}],
+    });
+if (!productData) {
+  res.status(404).json({ message: 'No product found with this id!' });
+  return;
+}
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+res.status(200).json(productData);
+} catch (err) {
+res.status(500).json(err);
+}
 });
 
-// create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
@@ -43,7 +37,6 @@ router.post('/', (req, res) => {
   */
   Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
@@ -53,7 +46,6 @@ router.post('/', (req, res) => {
         });
         return ProductTag.bulkCreate(productTagIdArr);
       }
-      // if no product tags, just respond
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
@@ -105,8 +97,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
